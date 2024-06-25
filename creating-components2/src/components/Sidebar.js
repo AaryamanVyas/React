5d657adde2da35df2fe4f06b682b99014b64cc13
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-06-25 18:36:47 +0000

    css

--- creating-components2/src/components/Sidebar.js
+++ creating-components2/src/components/Sidebar.js
@@ -1,5 +1,16 @@
-function Sidebar (props) {
-    return <h2>{props.greet} from Sidebar</h2>
+function Sidebar () {
+    aside {
+        background: azure;
+        width: calc(30% - 10px);
+        margin-left: 10px;
+      }
+    
+    return (
+        <aside className="sidebar-component">
+            <h2>Sidebar content here</h2>
+        </aside>
+    )
+    
 }
 
 export default Sidebar;
