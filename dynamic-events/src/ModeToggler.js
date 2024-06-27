f3fe6f4c87b59cf615433b3e6b5414bfca7f6ed0
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-06-27 17:16:26 +0000

    darkmode

--- dynamic-events/src/ModeToggler.js
+++ dynamic-events/src/ModeToggler.js
@@ -1,5 +1,12 @@
 function ModeToggler() {
-
+    const darkModeOn = true;
+    const darkMode = <h1>dark Mode is on</h1>
+    const lightMode = <h1>light Mode is on</h1>
+    return(
+        <div>
+            {darkModeOn ? darkMode : lightMode}
+        </div>
+    )
 }
 
 export default ModeToggler
