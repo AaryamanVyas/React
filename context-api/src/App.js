3588486f5c6d2b3801f6e6e5cd90f2659a5bac0d
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-06-29 10:21:37 +0000

    MealProvider.js

--- context-api/src/App.js
+++ context-api/src/App.js
@@ -1,9 +1,12 @@
 import MealsProvider.js from "./providers/MealsProvider";
-
+import MealsList from "./components/MealsList";
 import './App.css';
 
 function App() {
   return (
+    <MealsProvider >
+      <MealsList />
+      <Counter.js />
     
   );
 }
---
+++ context-api/src/providers/MealProvider.js
@@ -0,0 +1,19 @@
+import React, { Children } from "react";
+
+const MealsContext = React.createContext();
+
+const todaysMeals = ["Baked Beans", "Baked sweet Potatoes", "Baked Potatoes" ]
+
+const MealsProvider = ({Children}) => {
+    const [meals, setMealsList] = React.useState{todaysMeals}
+
+    return (
+        <MealsContext.Provider value={{meals}} >
+            {Children}
+        </MealsContext.Provider>
+    )
+}
+
+export const useMealsListContext = () => React.useContext
+
+export default MealsProvider
