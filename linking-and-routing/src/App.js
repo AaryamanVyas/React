f2330275ac95d511f6099077074e007974605b36
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-06-30 18:05:07 +0000

    final

--- linking-and-routing/src/App.js
+++ linking-and-routing/src/App.js
@@ -1,13 +1,14 @@
 import Homepage from './HomePage';
 import AboutMe from './AboutMe';
 import './App.css';
-import { Routes, Route, Link} from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
+
 function App() {
   return (
     <div className='App'>
       <nav className='nav'>
-        <link to="/" className='nav-ietms'>HomePage</link>
-        <link to="/about-me" className='nav-ietms'>About Me</link>
+        <Link to="/" className='nav-items'>HomePage</Link>
+        <Link to="/about-me" className='nav-items'>About Me</Link>
       </nav>
       <Routes>
         <Route path='/' element={<Homepage />} />
