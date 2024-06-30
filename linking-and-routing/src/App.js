0ed8228b87ebfec25e791b9d2c9219ae7c8b3b25
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-06-30 17:36:53 +0000

    bugfixes2

--- linking-and-routing/src/App.js
+++ linking-and-routing/src/App.js
@@ -1,19 +1,18 @@
 import Homepage from './HomePage';
 import AboutMe from './AboutMe';
 import './App.css';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link} from 'react-router-dom';
 function App() {
   return (
     <div className='App'>
       <nav className='nav'>
-      <link to="/" className='nav-ietms'>HomePage</link>
-      <link to="/about-me" className='nav-ietms'>About Me</link>
+        <link to="/" className='nav-ietms'>HomePage</link>
+        <link to="/about-me" className='nav-ietms'>About Me</link>
       </nav>
       <Routes>
-      <Route path='/' element={<Homepage />} />
-      <Route path='/about-me' element={<AboutMe />} />
+        <Route path='/' element={<Homepage />} />
+        <Route path='/about-me' element={<AboutMe />} />
       </Routes>
-      
     </div>
   );
 }
