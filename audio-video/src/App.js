7e80813b7c39ef3ddab8a65ffd11c41fa9e5b955
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-07-02 18:27:04 +0000

    App.js

--- audio-video/src/App.js
+++ audio-video/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from "react-player";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>React Player example</h1>
+      <ReactPlayer
+        url={vidUrl}
+        playing={false}
+        volume={0.5}
+      />
     </div>
   );
 }
